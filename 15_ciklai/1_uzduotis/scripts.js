3cc1ee0b8edde073e4a5a50f3905e8d83689fe4a
simonbrazer <simas.brazinskas@gmail.com>
2024-10-16 21:42:56 +0300

    newlessonsadded

---
+++ 15_ciklai/1_uzduotis/scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
+    for (let i = 0; i < fruits.length; i++) {
+        console.log(fruits[i]);
+    }    
+});  
